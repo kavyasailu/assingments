9fe419f00b2650acf4e15f5c5b03cfbe7cab9750
kavyasailu <138876200+kavyasailu@users.noreply.github.com>
2023-07-07 17:10:01 +0530

    Add files via upload

---
+++ AreaLen.java
@@ -0,0 +1,12 @@
+public class AreaLen{
+
+                 void Triarea(int base,int height){
+                         int area = (base*height)/2;
+                         System.out.println("Area of a Triangle "+ area);
+                     }
+                 void Recarea(int length,int breadth){
+                         int area = (length*breadth);
+                         System.out.println("Area of a Triangle "+ area);
+                     }
+
+}
