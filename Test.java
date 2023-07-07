9012bf6e8d530989cb9478950cd8987daff1cfba
kavyasailu <138876200+kavyasailu@users.noreply.github.com>
2023-07-07 17:00:17 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,10 @@
+import static com.abc.graphics.Calculator.*;
+public class Test
+{
+    public static void main(String[] args)
+{
+    //System.out.println(PI);
+    System.out.println(sum(12,12));
+    System.out.println(power(5,3));
+}
+}
