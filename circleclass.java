6ac06c27b4eaaaf1be200b41db5777a5be6b2379
kavyasailu <138876200+kavyasailu@users.noreply.github.com>
2023-07-07 17:05:24 +0530

    Add files via upload

---
+++ circleclass.java
@@ -0,0 +1,9 @@
+import com.abc.graphics.Rectangle;
+public class circleclass
+{
+   public static void main(String[] args)
+   {
+      Rectangle r1 = new Rectangle(7.5,6.7);
+      System.out.println(r1.area());
+   }
+}
